19e4c2da3d10728445033172d0f45cb230f2598b
Carol Li <caroldotli@gmail.com>
2020-04-22 10:57:46 -0400

    restaurant modal

--- yolp/restaurant/models.py
+++ yolp/restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-# Create your models here.
+PRICE_CHOICES = (
+    ('$','$'),
+    ('$$', '$$'),
+    ('$$$','$$$'),
+    ('$$$$','$$$$'),
+)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+
+class Restaurant(models.Model):
+    # managing user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # restaurant details
+    restaurant_name = models.CharField(max_length=200)
+    price = models.CharField(max_length=4, choices=PRICE_CHOICES, default='$')
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    categories = models.ManyToManyField(Category, blank=True)
+    location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now=True, null=True)
--- yolp/restaurant_admin/models.py
+++ yolp/restaurant_admin/models.py
@@ -14,11 +14,7 @@ class RestaurantAdmin(models.Model):
     # has user field
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    restaurant_name = models.CharField(max_length=200)
-    school = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    price = models.CharField(max_length=4, choices=PRICE_CHOICES, default='$')
-    description = models.TextField()
     time = models.DateTimeField(auto_now=True, null=True)
+    bio = models.TextField()
     is_student = False
     is_restaurant = True
